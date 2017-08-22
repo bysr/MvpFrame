f1641c0b159a9d7ccb39465de9338c8864c8bf07
wangyawen <1350212947@qq.com>
2017-08-22 16:39:31 +0800

    增加说明

--- app/src/main/java/com/wolearn/mvpframe/login/contract/LoginContract.java
+++ app/src/main/java/com/wolearn/mvpframe/login/contract/LoginContract.java
@@ -8,12 +8,17 @@ import com.wolearn.mvpframelib.base.BaseView;
  * Data: 2016/8/4.
  */
 public class LoginContract {
+
+    /*UI抽象类*/
     public interface View extends BaseView {
         String getAccount();
         String getPassword();
         void loginSuccess();
         void loginError(String errorMsg);
     }
+
+
+    //业务逻辑抽象类
 
     public interface Present extends BasePresenter {
         void login();
